dec96e695b5197047c5b8730079a303a1c8b5ac0
Soroush Baghernezhad <soroush.baghernezhad3@gmail.com>
2023-08-08 14:06:30 +0330

    hot fix

--- simple_transformer.py
+++ simple_transformer.py
@@ -50,6 +50,6 @@ model = NERModel(
 model.train_model(train,output_dir='./out_train/',show_running_loss=True)
 
 result, model_outputs, wrong_preds = model.eval_model(validation,output_dir = "./out_eval/",verbose= True)
-print("results : " + str(results))
+print("result : " + str(result))
 print("wrong preds" + str(wrong_preds))
 wandb.join()
