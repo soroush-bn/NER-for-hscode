310645009f6199670d529d11f4b192a5c757016b
Soroush Baghernezhad <soroush.baghernezhad3@gmail.com>
2023-08-09 17:23:57 +0330

    adding early stopping configs

--- simple_transformer.py
+++ simple_transformer.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import wandb
 
 
-df = pd.read_csv("NER.csv",error_bad_lines=False)
+df = pd.read_csv("NER_IOB.csv",error_bad_lines=False)
 df = df.dropna()
 df = df.drop(["Unnamed: 0"],axis = 1 )
 df["labels"]=df.labels.map(str)
@@ -21,7 +21,7 @@ print(cuda_available)
 wandb.login()
 wandb.init(
     # set the wandb project where this run will be logged
-    project="NER",
+    project="NER_IOB",
 
 )
 
@@ -29,11 +29,16 @@ wandb.init(
 model_args = NERArgs()
 model_args.labels_list = df.labels.unique().tolist()
 model_args.classification_report=True
-model_args.wandb_project = "NER"
+model_args.wandb_project = "NER_IOB"
 model_args.num_train_epochs=1000
 model_args.overwrite_output_dir = True
 model_args.reprocess_input_data = True
-
+model_args.use_early_stopping = True
+model_args.early_stopping_delta = 0.01
+model_args.early_stopping_metric = "mcc"
+model_args.early_stopping_metric_minimize = False
+model_args.early_stopping_patience = 5
+model_args.evaluate_during_training_steps = 1000
 
 
 model = NERModel(
@@ -47,9 +52,9 @@ model = NERModel(
 )
 
 
-model.train_model(train,output_dir='./out_train/',show_running_loss=True)
+model.train_model(train,output_dir='./out_train_ner_iob/',show_running_loss=True)
 
-result, model_outputs, wrong_preds = model.eval_model(validation,output_dir = "./out_eval/",verbose= True)
+result, model_outputs, wrong_preds = model.eval_model(validation,output_dir = "./out_eval_ner_iob/",verbose= True)
 print("result : " + str(result))
 print("wrong preds" + str(wrong_preds))
 wandb.join()
