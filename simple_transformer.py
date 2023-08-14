cc83500bba7d1f0ef1cb41119d4efd2ea7ab04df
Soroush Baghernezhad <soroush.baghernezhad3@gmail.com>
2023-08-14 11:46:10 +0330

    new ds new configs

--- simple_transformer.py
+++ simple_transformer.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import wandb
 
 
-df = pd.read_csv("NER_final.csv",error_bad_lines=False)
+df = pd.read_csv("bioNER_final.csv",error_bad_lines=False)
 df = df.dropna()
 df = df.drop(["Unnamed: 0"],axis = 1 )
 df.rename(columns={"tokens": "words", "tags": "labels"},inplace=True)
@@ -22,7 +22,7 @@ print(cuda_available)
 wandb.login()
 wandb.init(
     # set the wandb project where this run will be logged
-    project="NER_IOB",
+    project="NER_BIO",
 
 )
 
@@ -30,8 +30,8 @@ wandb.init(
 model_args = NERArgs()
 model_args.labels_list = df.labels.unique().tolist()
 model_args.classification_report=True
-model_args.wandb_project = "NER_IOB"
-model_args.num_train_epochs=1000
+model_args.wandb_project = "NER_BIO"
+model_args.num_train_epochs=500
 model_args.overwrite_output_dir = True
 model_args.reprocess_input_data = True
 model_args.use_early_stopping = True
@@ -39,7 +39,12 @@ model_args.early_stopping_delta = 0.01
 model_args.early_stopping_metric = "mcc"
 model_args.early_stopping_metric_minimize = False
 model_args.early_stopping_patience = 5
-model_args.evaluate_during_training_steps = 1000
+model_args.evaluate_during_training = True
+model_args.evaluate_during_training_steps = 100000
+model_args.evaluate_during_training_verbose = True
+model_args.save_steps = -1 
+model_args.save_model_every_epoch = True
+model_args.train_batch_size = 1000
 
 
 model = NERModel(
@@ -53,9 +58,9 @@ model = NERModel(
 )
 
 
-model.train_model(train,output_dir='./out_train_ner_iob/',show_running_loss=True)
+model.train_model(train,output_dir='./out_train_ner_bio/',show_running_loss=True)
 
-result, model_outputs, wrong_preds = model.eval_model(validation,output_dir = "./out_eval_ner_iob/",verbose= True)
+result, model_outputs, wrong_preds = model.eval_model(validation,output_dir = "./out_eval_ner_bio/",verbose= True)
 print("result : " + str(result))
 print("wrong preds" + str(wrong_preds))
 wandb.join()
