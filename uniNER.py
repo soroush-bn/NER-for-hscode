830c36a61507a4acf9cc8ddc0f282bf1e3e553d6
Soroush Baghernezhad <soroush.baghernezhad3@gmail.com>
2023-08-14 10:50:13 +0330

    adding UniNER model

--- uniNER.py
+++ uniNER.py
@@ -1,6 +1,15 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("Universal-NER/UniNER-7B-definition")
-model = AutoModelForCausalLM.from_pretrained("Universal-NER/UniNER-7B-definition")
+tokenizer = AutoTokenizer.from_pretrained("Universal-NER/UniNER-7B-type")
+model = AutoModelForCausalLM.from_pretrained("Universal-NER/UniNER-7B-type")
 
+#test
+
+# prompt = 'Given a passage, your task is to extract all entities and identify their entity types. The output should be in a list of tuples of the following format: [("entity 1", "type of entity 1"), ... ].'
+prompt = str(input("please enter the text : \n"))
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
