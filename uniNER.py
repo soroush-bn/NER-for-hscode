4acffe0a408aca3e9f7b3081cf1a624dc5ecda47
Soroush Baghernezhad <soroush.baghernezhad3@gmail.com>
2023-08-16 11:03:03 +0330

    hotfix

--- uniNER.py
+++ uniNER.py
@@ -11,7 +11,7 @@ def generate_from_model(text,model, tokenizer):
   encoded_input = tokenizer(text, return_tensors='pt')
   output_sequences = model.generate(input_ids=encoded_input['input_ids'].cuda(),max_new_tokens=max_new_tokens )
   print("out sequence is : " +  str(output_sequences))
-  return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
+  return tokenizer.decode(output_sequences[0], skip_special_tokens=True,clean_up_tokenization_spaces=False)
 
 
 examples = [{"conversations": [{"from": "human", "value": f"Text: I want some Fresh Kiwi Fruits"}, {"from": "gpt", "value": "I've read this text."}, {"from": "human", "value": f"What describes Product in the text?"}, {"from": "gpt", "value": "[]"}]}]
@@ -25,17 +25,17 @@ model_8bit = AutoModelForCausalLM.from_pretrained(name, device_map="auto", load_
 tokenizer = AutoTokenizer.from_pretrained(name)
 
 pipe = pipeline("text-generation", model="Universal-NER/UniNER-7B-type")
-recognizer = pipeline("ner", model=model_8bit, tokenizer=tokenizer)
+recognizer = pipeline("text-generation", model=model_8bit, tokenizer=tokenizer)
 text = ''
 while text != '0':
     text = str(input("enter your input : \n"))
     input_pipe =  prompt.format(text)
-    print("ur paragraph is :  \n" + str(input_pipe))
+    # print("ur paragraph is :  \n" + str(input_pipe))
     # result  =generate_from_model(input_pipe,model_8bit,tokenizer)
     result = pipe(text)
     print("the pipe result is : \n" + str(result))
     res2 = recognizer(text)
-    print("the pipe nerresult is : \n" + str(res2))
+    print("the pipe ner result is : \n" + str(res2))
 
     print()
     # prompt = 'Given a paragraph, your task is to extract all entities and concepts, and define their type using a short sentence. The output should be in the following format: [("entity", "definition of entity type in a short sentence"), ... ] the paragraph is : {}'
