ac75a6f58883526c0092aac4cca78147be6680c6
Soroush Baghernezhad <soroush.baghernezhad3@gmail.com>
2023-08-17 09:34:35 +0330

    hotfix Timer

--- timer.py
+++ timer.py
@@ -13,7 +13,7 @@ class Timer():
         if self._start_time is not None :  
             raise TimerError("you need to use .stop() before starting")
 
-        self._start_time = time.pref_counter()
+        self._start_time = time.perf_counter()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self._start_time is None:
